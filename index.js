45ca2c1a8390238955476c60d98b8b58f972a99d
Helen Liutongco <bellpickle@gmail.com>
2017-11-11 23:46:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,8 @@ function Driver(name,age,experience){
   this["age"] = age;
   this["experience"] = experience;
 }
+
+function PickUpLocation(address, city) {
+  this.address = address;
+  this.city = city;
+}
